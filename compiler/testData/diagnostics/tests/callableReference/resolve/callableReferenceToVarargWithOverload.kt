3000f0f1e570d59181dc098d6fa08e75f5b8a656
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2020-01-20 13:35:56 +0300

    WIP: test CR against varargs

---
+++ compiler/testData/diagnostics/tests/callableReference/resolve/callableReferenceToVarargWithOverload.kt
@@ -0,0 +1,15 @@
+// !LANGUAGE: +NewInference
+// !DIAGNOSTICS: -UNUSED_PARAMETER
+
+import kotlin.reflect.KFunction2
+
+class Foo
+
+fun test(fn: KFunction2<Foo, Array<out String>, String>) = null
+
+fun Foo.bar(vararg x: String) = ""
+fun Foo.bar(vararg x: Int) = ""
+
+fun test() {
+    test(Foo::bar)
+}

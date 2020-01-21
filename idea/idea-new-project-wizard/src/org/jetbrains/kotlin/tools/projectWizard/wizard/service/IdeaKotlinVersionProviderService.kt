c4617d01346c9f48d62295123c38bb8c94ee4dc7
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2020-01-21 15:10:29 +0300

    Wizard: cache latest stable Kotlin version

--- idea/idea-new-project-wizard/src/org/jetbrains/kotlin/tools/projectWizard/wizard/service/IdeaKotlinVersionProviderService.kt
+++ idea/idea-new-project-wizard/src/org/jetbrains/kotlin/tools/projectWizard/wizard/service/IdeaKotlinVersionProviderService.kt
@@ -5,6 +5,7 @@
 
 package org.jetbrains.kotlin.tools.projectWizard.wizard.service
 
+import com.intellij.ide.util.PropertiesComponent
 import org.jetbrains.kotlin.idea.KotlinPluginUtil
 import org.jetbrains.kotlin.idea.framework.ui.ConfigureDialogWithModulesAndVersion
 import org.jetbrains.kotlin.tools.projectWizard.core.asNullable
@@ -22,22 +23,48 @@ class IdeaKotlinVersionProviderService : KotlinVersionProviderService, IdeaWizar
         return listOf(version, getLatestStableOrDefault())
     }
 
-    private fun getListOfVersions() = safe {
-        ConfigureDialogWithModulesAndVersion
-            .loadVersions(MINIMUM_KOTLIN_VERSION_TO_LOAD)
-            .map(Version.Companion::fromString)
-    }.asNullable
-
-    override fun getCurrentKotlinVersion(): Version? {
-        if (KotlinPluginUtil.isSnapshotVersion()) return null
-        return Version.fromString(KotlinPluginUtil.getPluginVersion())
-    }
+    override fun getCurrentKotlinVersion(): Version? =
+        KotlinLatestStableVersionService.getCurrentKotlinVersion()?.let(Version.Companion::fromString)
 
     private fun getLatestStableOrDefault() =
-        getListOfVersions()?.firstOrNull()
+        KotlinLatestStableVersionService.getLatestStableKotlinVersion()
+            ?.let(Version.Companion::fromString)
             ?: KotlinVersionProviderServiceImpl.DEFAULT
+}
 
-    companion object {
-        private const val MINIMUM_KOTLIN_VERSION_TO_LOAD = "1.3.61"
+private object KotlinLatestStableVersionService {
+    private const val LATEST_STABLE_KOTLIN_VERSION_OPTION_NAME = "kotlin.wizard.latest.stable.version"
+    private const val LATEST_STABLE_KOTLIN_VERSION_SAVE_ON_VERSION_OPTION_NAME = "kotlin.wizard.latest.stable.version.saved.on"
+    private const val MINIMUM_KOTLIN_VERSION_TO_LOAD = "1.3.61"
+
+    fun getLatestStableKotlinVersion(): String? {
+        val cached = PropertiesComponent.getInstance()
+            .getValue(LATEST_STABLE_KOTLIN_VERSION_OPTION_NAME)
+            .takeIf { isCachedValueValid() }
+        if (cached != null) return cached
+        val latestStable = getListOfVersions()?.firstOrNull() ?: return null
+        PropertiesComponent.getInstance().setValue(LATEST_STABLE_KOTLIN_VERSION_OPTION_NAME, latestStable)
+        getCurrentKotlinVersion()?.let { current ->
+            PropertiesComponent.getInstance().setValue(LATEST_STABLE_KOTLIN_VERSION_SAVE_ON_VERSION_OPTION_NAME, current)
+        }
+        return latestStable
     }
+
+    fun getCurrentKotlinVersion(): String? {
+        if (KotlinPluginUtil.isSnapshotVersion()) return null
+        return KotlinPluginUtil.getPluginVersion()
+    }
+
+    private fun getListOfVersions() = safe {
+        ConfigureDialogWithModulesAndVersion.loadVersions(MINIMUM_KOTLIN_VERSION_TO_LOAD)
+    }.asNullable
+
+    private fun isCachedValueValid(): Boolean {
+        val pluginVersion = getCurrentKotlinVersion() ?: return false
+        val cachedVersionSavedOn = PropertiesComponent.getInstance()
+            .getValue(LATEST_STABLE_KOTLIN_VERSION_SAVE_ON_VERSION_OPTION_NAME)
+            ?: return false
+        return cachedVersionSavedOn == pluginVersion
+    }
+
 }
